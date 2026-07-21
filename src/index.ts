a345e3a8312396d1e50f72c5eeea461338f69b30
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-22 02:59:31 +0300

    Print board with a startng position in terminal

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,45 @@
 import { createStartPosition } from "./board";
+import { Color, PieceType} from "./types";
 
-createStartPosition()
+const letters: Record<PieceType, string> = {
+  [PieceType.Rook]: "r",
+  [PieceType.Knight]: "n",
+  [PieceType.Bishop]: "b",
+  [PieceType.Queen]: "q",
+  [PieceType.King]: "k",
+  [PieceType.Pawn]: "p"
+}
+
+function pieceNumberToLetter(pieceNumber: PieceType, color: Color): string {
+  let letter = ""
+
+  if (color === Color.White) {
+    return letter += letters[pieceNumber].toUpperCase()
+  } else  {
+    return letter += letters[pieceNumber]
+  }
+}
+
+const board = createStartPosition()
+
+let boardString = ""
+
+for (let rank = 7; rank >= 0; rank--) {
+  for (let file = 0; file <= 7; file++) {
+    let index = (rank * 8) + file
+
+    if (index % 8 === 0) {
+      console.log(boardString)
+      boardString = ""
+    }
+
+    let piece = board[index]
+
+    if (piece == null) {
+      boardString += "x "
+    } else {
+      boardString += pieceNumberToLetter(piece?.pieceType, piece?.color) + " "
+    }
+  }
+}
+console.log(boardString)
