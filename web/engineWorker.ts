7a68c26daa77ca7c786dd38c903f1056213c8ffd
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-24 04:03:56 +0300

    Add webworker to connect engine with a browser

---
+++ web/engineWorker.ts
@@ -0,0 +1,40 @@
+import { START_FEN } from "../src/constants";
+import { parseFEN } from "../src/position/fen";
+import { moveToNotation, notationToMove } from "../src/notation";
+import { makeLegalMove } from "../src/moves/makeMove";
+import { findBestMove } from "../src/search/search";
+import { type SearchParameters } from "../src/search/types";
+
+let position = parseFEN(START_FEN);
+let gameHistory: number[] = [position.hash];
+
+self.onmessage = (event: MessageEvent) => {
+  const data = event.data;
+
+  if (data.type === "move") {
+    const m = notationToMove(data.move, position);
+    const newPos = makeLegalMove(position, m);
+    if (newPos != null) {
+      gameHistory.push(newPos.hash);
+      position = newPos;
+    }
+  } else if (data.type === "go") {
+    const params: SearchParameters = {
+      name: "timeLimit",
+      limit: data.movetime,
+    };
+    const result = findBestMove(position, params, gameHistory);
+
+    if (result.bestMove !== undefined) {
+      const newPos = makeLegalMove(position, result.bestMove);
+      if (newPos != null) {
+        gameHistory.push(newPos.hash);
+        position = newPos;
+      }
+      self.postMessage({
+        type: "bestmove",
+        move: moveToNotation(result.bestMove),
+      });
+    }
+  }
+};
--- web/main.ts
+++ web/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello from web");
+const worker = new Worker(new URL("./engineWorker.ts", import.meta.url), {
+  type: "module",
+});
+
+worker.onmessage = (event) => {
+  console.log("received:", event.data);
+};
+
+worker.postMessage({ type: "go", movetime: 1000 });
